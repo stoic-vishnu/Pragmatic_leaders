811fc79eb231495a1747853048d888f2154d1486
Mahavishnu G <stoicvishnu@gmail.com>
2023-02-04 19:17:30 +0530

    Update LongestIncreasingSubsequence.java

--- LongestIncreasingSubsequence.java
+++ LongestIncreasingSubsequence.java
@@ -33,41 +33,13 @@ public class LongestIncreasingSubsequence {
             newT = actualSolution[t];
         } while (t != newT);
         System.out.println();
-
         return T[maxIndex];
-    }
-
-
-    //  * Recursive way of solving LIS
-    public int longestSubsequenceRecursive(int arr[]) {
-        int maxLen = 0;
-        for (int i = 0; i < arr.length - 1; i++) {
-            int len = longestSubsequenceRecursive(arr, i + 1, arr[i]);
-            if (len > maxLen) {
-                maxLen = len;
-            }
-        }
-        return maxLen + 1;
-    }
-
-    private int longestSubsequenceRecursive(int arr[], int pos, int lastNum) {
-        if (pos == arr.length) {
-            return 0;
-        }
-        int t1 = 0;
-        if (arr[pos] > lastNum) {
-            t1 = 1 + longestSubsequenceRecursive(arr, pos + 1, arr[pos]);
-        }
-        int t2 = longestSubsequenceRecursive(arr, pos + 1, lastNum);
-        return Math.max(t1, t2);
     }
 
     public static void main(String args[]) {
         LongestIncreasingSubsequence lis = new LongestIncreasingSubsequence();
         int arr[] = {1 , 3, 6, 7, 9, 4, 10, 5, 6 };
         int result = lis.longestSubsequenceWithActualSolution(arr);
-        int result1 = lis.longestSubsequenceRecursive(arr);
-        // System.out.println(result);
-        System.out.println(result1);
+        System.out.println(result);
     }
 }
